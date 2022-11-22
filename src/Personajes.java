391a8a97648285430b744d409d18afeb61505438
rafaelperez0101 <rafael.perez01@telusinternational.com>
2022-11-21 18:59:08 -0600

    AgregandoPersonajes

--- src/Personajes.java
+++ src/Personajes.java
@@ -1,7 +1,52 @@
 package src;
 
+
 public abstract class Personajes {
 
+    protected String name;
+    protected Integer ataque;
+    protected Integer armadura;
+    protected Integer vida;
 
+    public Personajes(){
 
+    }
+    public Personajes(String name, Integer ataque, Integer armadura, Integer vida) {
+        this.name = name;
+        this.ataque = ataque;
+        this.armadura = armadura;
+        this.vida = vida;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(Integer ataque) {
+        this.ataque = ataque;
+    }
+
+    public Integer getArmadura() {
+        return armadura;
+    }
+
+    public void setArmadura(Integer armadura) {
+        this.armadura = armadura;
+    }
+
+    public Integer getVida() {
+        return vida;
+    }
+
+    public void setVida(Integer vida) {
+        this.vida = vida;
+    }
 }
