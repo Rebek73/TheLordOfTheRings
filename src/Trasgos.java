5a426ffe41e21bb5abd793c731e51b8cbf9cdf10
Rebecca <rebemon95@outlook.com>
2022-11-23 11:52:15 -0600

    Update Trasgos.java

--- src/Trasgos.java
+++ src/Trasgos.java
@@ -3,12 +3,13 @@ package src;
 public class Trasgos extends Bestias{
 
     public Trasgos(){
-
+        super();
     }
 
     public Trasgos(String nombre, int ataque, int armadura, int vida){
-        super();
+        super(nombre, ataque, armadura, vida);
     }
+    
     @Override
     public void tirarDados(){
         super.tirarDados();
