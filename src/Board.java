e65511376e116c88a20e3e9bd48c9103a428bc32
rafaelperez0101 <rafael.perez01@telusinternational.com>
2022-11-25 13:02:20 -0600

    Ultimos cambios

---
+++ src/Board.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Board {
+
+    private  String ganador;
+    private String perdedor;
+
+
+    public String win(String ganador){
+        return ganador;
+
+    }
+
+}
+
